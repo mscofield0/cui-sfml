412ca2edb2c413b0b748e2ff8cb38d24da0aa3f0
Roko Burilo <roko.burilo1@skole.hr>
2020-03-29 03:56:06 +0200

    Added SFML

---
+++ compile.bat
@@ -0,0 +1,2 @@
+@echo off
+cmake --build . --target CUI-SFML -- -j 2
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <utility>
+
+int main() {
+  auto win =
+      std::make_unique<sf::RenderWindow>(sf::VideoMode(800, 600), "Title");
+
+  while (win->isOpen()) {
+    // check all the window's events that were triggered since the last
+    // iteration of the loop
+    sf::Event event;
+    while (win->pollEvent(event)) {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed)
+        win->close();
+    }
+
+    // clear the window with black color
+    win->clear(sf::Color::Black);
+
+    // draw everything here...
+    // window.draw(...);
+
+    // end the current frame
+    win->display();
+  }
+}
