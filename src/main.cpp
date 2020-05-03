d137820dec939fffb7411097fcfd421baa8e5278
Roko Burilo <roko.burilo1@skole.hr>
2020-05-03 05:38:10 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@
 #define STATIC_STRING_HOLDER(name) static constexpr const char name[] =
 #define END_STATIC_STRING_HOLDER ;
 
+void edit_button_positions(cui::SceneGraph& graph);
+
 int main() {
 	using namespace cui;
 
@@ -37,24 +39,12 @@ int main() {
 #include "file.scene"
 	END_STATIC_STRING_HOLDER
 
-	STATIC_STRING_HOLDER(message_window_style__)
-#include "message_window.styles"
-	END_STATIC_STRING_HOLDER
-
-	STATIC_STRING_HOLDER(message_window_scene__)
-#include "message_window.scene"
-	END_STATIC_STRING_HOLDER
-
 	// Typedefs for easy use
 	using win_t = Window;
 	using node_t = Node;
 	using event_data_t = EventData<node_t>;
 
 	std::unique_ptr<win_t> window;
-	std::unique_ptr<win_t> message_window;
-	static auto rand_device = std::make_unique<std::random_device>();
-	static auto mt_rand = std::make_unique<std::mt19937>((*rand_device)());
-	static std::uniform_int_distribution rand_255(0, 255);
 
 	// Create a scope so it destructs at the end
 	{
@@ -93,78 +83,64 @@ int main() {
 		}
 	}
 
-	// Create a scope so it destructs at the end
-	{
-		// Parse the .scene file
-		constexpr auto scenes_variant = ct::scenes::parse_scenes<message_window_scene__>();
-		// Handle if error
-		if constexpr (scenes_variant.is_type_b()) {
-			println(scenes_variant.type_b());
-			return 0;
-		}
-
-		// Parse the .styles file
-		constexpr auto styles_variant = ct::styles::parse_styles<message_window_style__>();
-		// Handle if error
-		if constexpr (styles_variant.is_type_b()) {
-			println(styles_variant.type_b());
-			return 0;
-		}
-		// Populate the Window with the parsed data
-		else
-		{
-			std::vector<ct::Style> sty;
-			sty.reserve(styles_variant.type_a().size());
-			for (const auto& el : styles_variant.type_a()) {
-				const auto parsed_variant = ct::Style::create(el);
-				if (parsed_variant.is_type_b()) {
-					println(parsed_variant.type_b());
-					return 0;
-				}
-				const auto& parsed = parsed_variant.type_a();
-				sty.push_back(parsed);
-			}
-
-			// Create the Window
-			message_window = std::make_unique<win_t>(std::move(sty), std::move(scenes_variant.type_a()));
-		}
-	}
-
-	window->event_cache["is_window_opened"] = false;
+	constexpr auto text_box_index = 0;
+	edit_button_positions(window->active_scene().graph());
 
 	// Create a typedef for the event types
 	using EventType = sf::Event::EventType;
 
 	// Register the on_close event [OBLIGATORY, otherwise you cannot close via conventional methods]
 	window->register_global_event(EventType::Closed, "on_close", [&window](auto event_data) { window->close(); });
-	message_window->register_global_event(EventType::Closed, "on_close", [&window](auto event_data) { window->close(); });
 
 	// Register the on_resize event [OPTIONAL, but a resize event is one of the events that should update the scene graph]
 	window->register_global_event(EventType::Resized, "on_resize", [&window](auto event_data) { templates::OnResize((*window), event_data); });
 
 	// Register the on_click_btn event [OPTIONAL, defines functionality on button click]
-	window->register_event(EventType::MouseButtonPressed, "on_click_btn", [&window, &message_window](event_data_t event_data) {
+	window->register_event(EventType::MouseButtonPressed, "on_click_btn", [&window](event_data_t event_data) {
 		// Uses CUI's GUI helper template `OnClick` to provide functionality on click
 		println("Caller index:", event_data.caller_index());
 
-		templates::OnClick((*window), event_data, [&message_window](Window& window, event_data_t& event_data) {
+		templates::OnClick((*window), event_data, [](Window& window, event_data_t& event_data) {
 			auto& graph = window.active_scene().graph();
-
-			auto& is_window_opened = std::any_cast<bool&>(window.event_cache["is_window_opened"]);
-
-			if (is_window_opened) return;
-
-			println("Before initializing the other window");
-
-			is_window_opened = true;
-			message_window->init({800, 600, "Message window", sf::Style::Titlebar | sf::Style::Close, sf::ContextSettings{}, 60});
+			auto& node = graph[text_box_index].data();
+			node.text().append(event_data.caller()->text());
 		});
-	});	
+	});
 
 	// Attaches the registered event `on_click_btn` to the button nodes
-	window->attach_event_to_node("button", "on_click_btn");
+	window->attach_event_to_node("one", "on_click_btn");
+	window->attach_event_to_node("two", "on_click_btn");
+	window->attach_event_to_node("three", "on_click_btn");
+	window->attach_event_to_node("four", "on_click_btn");
+	window->attach_event_to_node("five", "on_click_btn");
+	window->attach_event_to_node("six", "on_click_btn");
+	window->attach_event_to_node("seven", "on_click_btn");
+	window->attach_event_to_node("eight", "on_click_btn");
+	window->attach_event_to_node("nine", "on_click_btn");
+	window->attach_event_to_node("zero", "on_click_btn");
 
 	// Initialize the window
 	window->init({800, 600, "Title", sf::Style::Default, sf::ContextSettings{}, 60});
-	window.reset(nullptr);
+}
+
+void edit_button_positions(cui::SceneGraph& graph) {
+	for (std::size_t i = 0; i < graph.length() - 2; ++i) {
+		const auto idx = i + 2;
+		auto& node = graph[idx].data();
+		auto& ascheme = node.active_schematic().get();
+
+		int x = 0;
+		float y = 0;
+
+		if (i <= 5) {
+			x = i * 60;
+			y = 0.f;
+		} else {
+			x = i * 60 + 40;
+			y = 0.5f;
+		}
+
+		ascheme.x() = x;
+		ascheme.y() = y;
+	}
 }
